2762ec51ceeb163259ea7b75d8380c010594c4b9
Torma Kristóf <tormakristof@tormakristof.eu>
2022-04-13 19:59:37 +0200

    update run config

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ansible-galaxy collection install -r requirements.yaml
-ansible-playbook --ask-become-pass -i inventory.yaml test.yaml
+ansible-playbook --ask-become-pass -i inventory.yaml $1 --ask-pass
