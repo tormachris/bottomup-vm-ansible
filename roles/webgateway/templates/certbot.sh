5cfc600b37551a73ff5ac455c68b25720e853056
Torma Kristóf <tormakristof@tormakristof.eu>
2022-04-14 21:41:04 +0200

    add web gateway stuff

---
+++ roles/webgateway/templates/certbot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# {{ansible_managed}}
+{% for proxysite in proxy %}
+certbot renew --nginx --cert-name {{proxysite.domain}}
+{% endfor %}
+{% for staticsite in static %}
+certbot renew --nginx --cert-name {{staticsite.domain}}
+{% endfor %}
