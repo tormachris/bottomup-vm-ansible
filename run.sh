8795639cbbea4e0beba16f0bcbd869f49b3e7aab
Kristóf Torma <tormakristof@tormakristof.eu>
2023-03-05 18:33:40 +0100

    fix envvar

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ansible-galaxy collection install -r requirements.yaml
 #ansible-playbook --ask-become-pass --ask-pass -i inventory.yaml $1
-ANSIBLE_CONFIG: "$SCRIPT_DIR/ansible.cfg" ansible-playbook -i inventory.yaml $1
+ANSIBLE_CONFIG="$SCRIPT_DIR/ansible.cfg" ansible-playbook -i inventory.yaml $1
