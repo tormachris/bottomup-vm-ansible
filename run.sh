0c7aee832c3873b645165d078a0184a31fe70007
Kristóf Torma <tormakristof@tormakristof.eu>
2023-03-05 18:32:21 +0100

    apply ansible config

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 ansible-galaxy collection install -r requirements.yaml
-ansible-playbook --ask-become-pass --ask-pass -i inventory.yaml $1
-#ansible-playbook --ask-become-pass -i inventory.yaml $1
+#ansible-playbook --ask-become-pass --ask-pass -i inventory.yaml $1
+ANSIBLE_CONFIG: "$SCRIPT_DIR/ansible.cfg" ansible-playbook -i inventory.yaml $1
